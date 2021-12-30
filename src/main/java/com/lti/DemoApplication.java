65ae7c717fbac9cb5919d8e611fdbcd77062449d
bhavanisollu <51796920+bhavanisollu@users.noreply.github.com>
2021-12-30 12:57:11 +0530

    last sent added

--- src/main/java/com/lti/DemoApplication.java
+++ src/main/java/com/lti/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	
 	@GetMapping("/demo")
 	public String getMessage() {
-		return "Welcome to jenkins CI/CD pipeline.On Minikube cluster";
+		return "Welcome to jenkins CI/CD pipeline.On Minikube cluster. A single node cluster.";
 	}
 
 	public static void main(String[] args) {
